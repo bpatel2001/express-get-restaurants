8e939c8541856fa8f9460e2ab0c1f948bb80a4f9
bpatel2001 <125521942+bpatel2001@users.noreply.github.com>
2025-05-19 10:22:17 -0500

    Finished activity

--- src/app.js
+++ src/app.js
@@ -5,4 +5,9 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below:
 
+app.get("/restaurants", async (request, response) => {
+    const restaurants = await Restaurant.findAll();
+    response.json(restaurants);
+});
+
 module.exports = app;
