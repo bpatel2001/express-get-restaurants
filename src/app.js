7684609c6a3c335fe1e359fa99ae6e08b1e6d7e0
bpatel2001 <bpatel2001h@gmail.com>
2025-05-20 10:32:44 -0500

    added create, update and delete endpoints

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 //TODO: Create your GET Request Route Below:
 
 app.get("/restaurants", async (request, response) => {
@@ -14,6 +17,32 @@ app.get("/restaurants/:id", async (request, response) => {
     const id = request.params.id;
 
     const restaurant = await Restaurant.findByPk(id);
+
+    response.json(restaurant);
+})
+
+app.post("/restaurants", async (request, response) => {
+    const restaurant = await Restaurant.create(request.body);
+
+    response.json(restaurant);
+})
+
+app.put("/restaurants/:id", async (request, response) => {
+    const restaurantId = request.params.id;
+
+    const restaurant = await Restaurant.update(request.body, {
+        where: { id: restaurantId }
+    });
+    
+    response.json(restaurant);
+})
+
+app.delete("/restaurants/:id", async (request, response) => {
+    const restaurantId = request.params.id;
+
+    const restaurant = await Restaurant.destroy({
+        where: { id: restaurantId }
+    });
 
     response.json(restaurant);
 })
